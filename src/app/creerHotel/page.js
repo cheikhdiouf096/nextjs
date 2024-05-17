5be3cf1d96f26786c41828ea23efa84ffbf969c2
cheikh diouf <cheikhdiouf096@gmail.com>
2024-05-17 14:47:18 +0000

    ajouter .js

--- src/app/creerHotel/page.js
+++ src/app/creerHotel/page.js
@@ -21,7 +21,7 @@ import {
   StyledSubmitCreer,
   FlexEnd,
   SpanAjouterPhoto
-} from "../creerHotel/CreerStyle";
+} from "../creerHotel/CreerStyle.js";
 import Link from "next/link";
 import Image from "next/image";
 
--- src/app/forgotpwd/page.js
+++ src/app/forgotpwd/page.js
@@ -4,7 +4,7 @@ import iconRed from "../assets/icon.png";
 import Image from "next/image";
 import Link from "next/link";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { StyledBackToLoginLink, StyledButton, StyledContainer,  StyledForm, StyledFormContainer, StyledFormInput, StyledFormLabel, StyledIcon, StyledInfo, StyledInput, StyledLogoContainer, StyledText, StyledTextInfo } from "../connexion/ConnexionStyle";
+import { StyledBackToLoginLink, StyledButton, StyledContainer,  StyledForm, StyledFormContainer, StyledFormInput, StyledFormLabel, StyledIcon, StyledInfo, StyledInput, StyledLogoContainer, StyledText, StyledTextInfo } from "../connexion/ConnexionStyle.js";
 
 
 const ForgotPwd = () => {
